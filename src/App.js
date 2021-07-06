e59bbb574e54da8b3056e621a871e164d6406ed4
Alexander Urrutia <matias04041994@gmail.com>
2021-07-06 12:48:06 -0400

    incluir componentes de estilos globales

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./theme/sass/style.scss";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Headline 1</h1>
+      <h2>Headline 2</h2>
+      <h3>Headline 3</h3>
+      <h4>Headline 4</h4>
+      <h5>Headline 5</h5>
+      <p class="font-regular">lorem ips</p>
+      <p class="font-regular-bold">lorem ips</p>
+      <p class="font-regular-second-bold">lorem ips</p>
+      <p class="font-regular-second-bold">lorem ips</p>
+    </>
   );
-}
+};
 
 export default App;
