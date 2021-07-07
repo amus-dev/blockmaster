3cf7d6beedc9adbb366bb56dca1e7de2ff9b779e
Alexander Urrutia <matias04041994@gmail.com>
2021-07-07 16:35:36 -0400

    Se agregan variables de entorno

--- src/utils/api/const.js
+++ src/utils/api/const.js
@@ -1,6 +1,3 @@
 export const API_HOST = process.env.REACT_APP_API_HOST;
 export const LANG = process.env.REACT_APP_LANG;
-export const API_KEY =
-  process.env.NODE_ENV === "production"
-    ? process.env.API_KEY_PRODUCTION
-    : process.env.REACT_APP_API_KEY;
+export const API_KEY = process.env.REACT_APP_API_KEY;
