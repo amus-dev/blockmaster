3b92913e4b78fe43464314fad5fb701c73884a17
Alexander Urrutia <matias04041994@gmail.com>
2021-07-07 15:43:30 -0400

    Se agregan variables de entorno

--- src/utils/api/const.js
+++ src/utils/api/const.js
@@ -1,3 +1,6 @@
 export const API_HOST = "https://api.themoviedb.org/3";
-export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_KEY =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_VERCEL_ENV
+    : process.env.REACT_APP_API_KEY;
 export const LANG = "es-ES";
