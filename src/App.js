89e5428c6fa1e4209df381c90203cf7761a18249
Alexander Urrutia <matias04041994@gmail.com>
2021-07-07 22:21:26 -0400

    Se agregan variables de entorno

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./theme/sass/style.scss";
 
 const App = () => {
+  console.log(process.env);
   return (
     <div className="App">
       <Routes />
