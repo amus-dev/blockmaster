79320df12b07c05e8b7226ecbe47357c00ed4132
Alexander Urrutia <matias04041994@gmail.com>
2021-07-07 22:24:58 -0400

    Se agregan variables de entorno

--- src/utils/api/const.js
+++ src/utils/api/const.js
@@ -1,5 +1,8 @@
+export const API_KEY =
+  process.env.NODE_ENV === "production"
+    ? process.env.REACT_APP_VERCEL_API_KEY
+    : process.env.REACT_APP_API_KEY;
 export const API_HOST = process.env.REACT_APP_API_HOST;
 export const LANG = process.env.REACT_APP_LANG;
-export const API_KEY = process.env.REACT_APP_API_KEY;
 export const BASE_URL_BANNER =
   "https://image.tmdb.org/t/p/w1920_and_h427_multi_faces/";
