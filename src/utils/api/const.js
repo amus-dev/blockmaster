5c008a104c24129fe9cde5d06d3af12a79a6f6f3
Alexander Urrutia <matias04041994@gmail.com>
2021-07-12 12:27:07 -0400

    editar variable de entorno

--- src/utils/api/const.js
+++ src/utils/api/const.js
@@ -1,4 +1,4 @@
-export const API_KEY = "f175e530c38760bab52d4d1626db82c6";
+export const API_KEY = process.env.REACT_APP_API_KEY;
 export const API_HOST = process.env.REACT_APP_API_HOST;
 export const LANG = process.env.REACT_APP_LANG;
 export const BASE_URL_BANNER =
