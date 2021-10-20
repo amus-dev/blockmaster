52c491b549f503114dfae76f2c2d752edb7ebbe6
Alexander Urrutia <aurrutia.externo@coopeuch.cl>
2021-10-20 18:42:25 -0300

    Change path url poster

--- src/utils/api/const.js
+++ src/utils/api/const.js
@@ -1,5 +1,4 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
 export const API_HOST = process.env.REACT_APP_API_HOST;
 export const LANG = process.env.REACT_APP_LANG;
-export const BASE_URL_BANNER =
-  "https://image.tmdb.org/t/p/w1920_and_h427_multi_faces/";
+export const BASE_URL_BANNER = "https://image.tmdb.org/t/p/original/";
